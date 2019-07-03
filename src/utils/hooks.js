880d5d7efa54fa55d8c957e764ab31e82e8813b1
joaogsleite <joaogsleite@gmail.com>
2019-07-03 12:23:41 +0100

    fixed useApi hook

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -16,20 +16,20 @@ export function useDebounce(input, debounce = 500) {
   return value
 }
 
-export function useApi(func, args) {
+export function useApi(func, ...args) {
   const [data, setData] = useState()
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(false)
-
+  
   useEffect(() => {
     setError(false)
     setLoading(true)
-    func(args).then(setData).catch(() => {
+    func(...args).then(setData).catch(() => {
       setError(true)
     }).then(() => {
       setLoading(false)
     })
-  }, [func, args])
+  }, [func, ...args])
 
   return [
     data,
